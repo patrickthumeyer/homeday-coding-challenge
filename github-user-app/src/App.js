1fdf99f90e9f60c46411ba7252c320919619abc6
patrickthumeyer <p.thumeyer@gmail.com>
2020-06-18 23:23:37 +0200

    add login form, fetch user and pass userdata via props.history.push to user-details page

--- github-user-app/src/App.js
+++ github-user-app/src/App.js
@@ -2,16 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./App.scss";
-import LoginForm from "./pages/login-form";
-import UserDetails from "./pages/user-details";
+import LoginForm from "./pages/login-form/login-form";
+import UserDetails from "./pages/user-details/user-details";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/user-details" component={LoginForm}></Route>
-          <Route exact path="/" component={UserDetails}></Route>
+          <Route path="/results/:user" component={UserDetails}></Route>
+          <Route exact path="/" component={LoginForm}></Route>
         </Switch>
       </Router>
     </div>
--- github-user-app/src/pages/login-form/login-form.js
+++ github-user-app/src/pages/login-form/login-form.js
@@ -2,10 +2,55 @@ import React from "react";
 import "./login-form.scss";
 
 class LoginForm extends React.Component {
-  state = {};
+  state = {
+    username: "",
+    userData: {},
+  };
+
+  onChangeHandler = (e) => {
+    this.setState({ username: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.getUserData();
+  };
+
+  // to fetch data I had to use api and not developer.github
+
+  // since component would have been unmounted before I can get data from the state I add the data as
+  // additional property on location in order to pass the userData to the UserDetails Page
+
+  async getUserData() {
+    const url = `https://api.github.com/users/${this.state.username}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    this.setState({ userData: data });
+    this.props.history.push({
+      pathname: `/results/${data.login}`,
+      data: data,
+    });
+  }
 
   render() {
-    return <div></div>;
+    console.log(this.state.username);
+
+    return (
+      <main>
+        <h1>Welcome to GitHub User Info</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.username}
+            onChange={this.onChangeHandler}
+            placeholder="Please enter GitHub username"
+            required
+          />
+          <input type="submit" value="Submit" />
+        </form>
+      </main>
+    );
   }
 }
 
